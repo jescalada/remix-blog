0a73d0ac5e58276aeb2d89c4c09e84e81b78752a
Juan Escalada <juanescalada175@gmail.com>
2023-01-01 16:20:50 -0800

    Add basic posts functionality

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,18 +1,11 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { getPosts } from "~/models/post.server";
 
 export const loader = async () => {
   return json({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I made just for you",
-      },
-    ],
+    posts: await getPosts(),
   });
 };
 
@@ -22,6 +15,15 @@ export default function Posts() {
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
